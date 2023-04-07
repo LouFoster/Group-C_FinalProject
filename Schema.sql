960ad59c9a327b30048f2787566b0d7cbff33060
jayhawkerfan84 <116672574+jayhawkerfan84@users.noreply.github.com>
2023-04-06 19:08:48 -0500

    Add files via upload
    Schema SQL for project database

---
+++ Schema.sql
@@ -0,0 +1,48 @@
+drop table if exists city
+drop table if exists air_pollution_by_city
+drop table if exists air_quality_lookup
+
+--Create city table
+CREATE TABLE city(
+  id serial PRIMARY KEY NOT NULL,
+  Country TEXT,
+  City text,
+  Population INT,
+  Latitude decimal,
+  Longitude decimal
+);
+
+-- Create Air Pollution Table
+CREATE TABLE air_pollution_by_city (
+  id serial PRIMARY KEY NOT NULL,
+  city TEXT,
+  date date,
+  air_quality_index INT,
+  so2 decimal,
+  no2 decimal,
+  pm10 decimal,
+  pm2point5 decimal, 
+  o3 decimal,
+  co decimal
+);
+
+-- Create Air Pollution Lookup Table
+CREATE TABLE air_quality_lookup (
+  id serial PRIMARY KEY NOT NULL,
+  air_quality_description text,
+  air_quality_index int	
+);
+
+--Add City_Id to Air Pollution Table
+ALTER TABLE air_pollution_by_city
+ADD COLUMN city_id int ;
+
+--Add Constraint
+Alter Table air_pollution_by_city
+ADD CONSTRAINT fk_city
+FOREIGN KEY (city_id)
+REFERENCES city(id)
+
+--Drop City from Air_Pollution_By_City
+ALTER TABLE air_pollution_by_city
+DROP Column city
